86863e183074f53f14b93b568bcaf79dfc297fe7
Mingyu Gao <mgao12@stanford.edu>
2015-12-30 22:28:02 -0800

    [utils] Use barrier with C++11 mutex and cv.

--- include/utils/threads.h
+++ include/utils/threads.h
@@ -10,9 +10,12 @@
 #include <mutex>
 #include <thread>
 
+class barrier;
+
 using thread_t = std::thread;
 using lock_t = std::mutex;
 using cond_t = std::condition_variable;
+using bar_t = barrier;
 
 
 /* Threads */
@@ -45,6 +48,60 @@ using cond_t = std::condition_variable;
  *  void notify_one();
  *  void notify_all();
  */
+
+
+/* Barrier */
+class barrier {
+    public:
+        static constexpr int SERIAL_LAST_THREAD = 1;
+
+        /**
+         * Construct the barrier.
+         *
+         * @param threadCount   The number of threads involved in the barrier.
+         */
+        explicit barrier(const std::size_t threadCount)
+            : threadCount_(threadCount), remain_(threadCount), barCount_(0)
+        {
+            // Nothing else to do.
+        }
+
+        /**
+         * Wait on the barrier.
+         *
+         * @return      SERIAL_LAST_THREAD if the thread is the last one arriving
+         *              at the barrier, or 0 otherwise.
+         */
+        int wait() {
+            {
+                std::unique_lock<std::mutex> lock(mutex_);
+                auto curBarCount = barCount_;
+                remain_--;
+                if (remain_) {
+                    // Not all threads have arrived, wait.
+                    // Wait predicate should not depend on \c remain_, since it must
+                    // be reset at the time all threads arriving at the barrier.
+                    cv_.wait(lock, [this, &curBarCount]{ return barCount_ != curBarCount; });
+                    return 0;
+                }
+            }
+            // Only executed by the very last thread arriving at the barrier.
+            // Reset \c remain_.
+            remain_ = threadCount_;
+            // Increase \c barCount_, which is used as the wait predicate.
+            barCount_++;
+            // Notify all after updating wait predicate, should not hold the mutex.
+            cv_.notify_all();
+            return SERIAL_LAST_THREAD;
+        }
+
+    private:
+        std::mutex mutex_;
+        std::condition_variable cv_;
+        const std::size_t threadCount_;
+        std::size_t remain_;
+        std::size_t barCount_;
+};
 
 #endif // UTILS_THREADS_H_
 
