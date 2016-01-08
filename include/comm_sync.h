6837d0d155eb8a29b296c8a45b8676c027a9aed6
Mingyu Gao <mgao12@stanford.edu>
2016-01-07 22:17:25 -0800

    [comm_sync] Declare key-value pair communication routines.

--- include/comm_sync.h
+++ include/comm_sync.h
@@ -1,6 +1,7 @@
 #ifndef COMM_SYNC_H_
 #define COMM_SYNC_H_
 
+#include "utils/stream.h"
 #include "utils/threads.h"
 
 template<typename KType, typename VType>
@@ -31,6 +32,17 @@ public:
         ValType val_;
     };
 
+    enum RecvStatusType {
+        // Nothing is received.
+        RECV_NONE,
+        // Some data is received, but not finished.
+        RECV_CONTINUED,
+        // All data is received.
+        RECV_FINISHED,
+    };
+
+    typedef Stream<KeyValue> KeyValueStream;
+
 public:
     explicit CommSync(const uint32_t threadCount)
         : threadCount_(threadCount), bar_(threadCount),
@@ -38,6 +50,11 @@ public:
     {
         // Nothing else to do.
     }
+
+    /**
+     * Number of threads.
+     */
+    uint32_t threadCount() const { return threadCount_; }
 
     /**
      * Synchronization barrier.
@@ -58,6 +75,43 @@ public:
         bar_.wait(scb);
         return barANDLastResult_;
     }
+
+    /**
+     * Send a key-value pair from \c prodId to \c consId.
+     */
+    void keyValNew(const uint32_t prodId, const uint32_t consId,
+            const KeyType& key, const ValType& val);
+
+    /**
+     * Send end-of-message tag from \c prodId to \c consId.
+     */
+    void endTagNew(const uint32_t prodId, const uint32_t consId);
+
+    /**
+     * Delete all key-value pairs associated with \c prodId at producer side
+     * after communication is done.
+     */
+    void keyValProdDelAll(const uint32_t prodId);
+
+    /**
+     * Delete all key-value pairs associated with \c consId at consumer side
+     * after communication is done.
+     */
+    void keyValConsDelAll(const uint32_t consId);
+
+    /**
+     * Receive all key-value pairs available now, and partition them into subpartitions.
+     *
+     * @param consId            Thread index of the consumer.
+     * @param partitionCount    Number of subpartitions.
+     * @param partitionFunc     Partition function. For a key \c k, partition index
+     *                          is calculated as <tt>partitionFunc(k) % partitionCount</tt>.
+     *
+     * @return      A pair consisting of the subpartitions, and the receiving status.
+     */
+    std::pair<std::vector<KeyValueStream>, RecvStatusType> keyValPartitions(
+            const uint32_t consId, const size_t partitionCount,
+            std::function<size_t(const KeyType&)> partitionFunc);
 
 private:
     const uint32_t threadCount_;
